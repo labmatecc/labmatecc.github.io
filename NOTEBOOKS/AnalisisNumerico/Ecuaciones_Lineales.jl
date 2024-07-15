0a447917ade892758820e3b3f30bb36bcc6a5eff
ytrujillol <ytrujillol@unal.edu.co>
2024-07-15 13:14:40 -0500

    update

--- NOTEBOOKS/AnalisisNumerico/Ecuaciones_Lineales.jl
+++ NOTEBOOKS/AnalisisNumerico/Ecuaciones_Lineales.jl
@@ -17,7 +17,7 @@ end
 PlutoUI.TableOfContents(title="Ecuaciones Lineales", aside=true)
 
 # ╔═╡ 5d0d4c4e-b432-437e-9b8e-2f81c79f907e
-md"""Este cuaderno esta en construcción y puede ser modificado en el futuro para mejorar su contenido. En caso de comentarios o sugerencias por favor escribir a **labmatecc_bog@unal.edu.co**
+md"""Este cuaderno está en construcción y puede ser modificado en el futuro para mejorar su contenido. En caso de comentarios o sugerencias, por favor escribir a **labmatecc_bog@unal.edu.co**
 
 Tu participación es fundamental para hacer de este curso una experiencia aún mejor."""
 
